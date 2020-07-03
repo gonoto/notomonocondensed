41a2ea221dda18e56593914e6da238be7f3ded24
Nik <njunger@uwaterloo.ca>
2020-07-03 13:01:29 -0400

    Initial commit

---
+++ chunk.go
@@ -0,0 +1,4 @@
+package notomonocondensed
+
+var chunks = [][]uint64{chunk0, chunk1, chunk2}
+const decompressedSize = 64728316
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/gonoto/notomonocondensed
+
+go 1.14
